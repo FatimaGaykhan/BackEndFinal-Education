b8ad8dd0c1c2846209417e5317017ebc56ee490a
FatimaGaykhan <fatimaeg@code.edu.az>
2024-06-05 12:11:44 +0400

    Added First Commit

---
+++ Back-End-Final-Education/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Back_End_Final_Education.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
